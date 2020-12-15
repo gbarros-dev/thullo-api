02c0cb1915c9687e2e7dded407b9514b13d0660b
Guilherme Barros <hello@gbarros.dev>
2020-12-15 10:52:53 -0300

    Assignment controller minor correction

--- src/controllers/AssignmentController.ts
+++ src/controllers/AssignmentController.ts
@@ -2,44 +2,19 @@ import { Response } from 'express'
 import * as httpStatus from 'http-status'
 
 import { Request } from '../utils/types/common'
+import stringToObjectId from '../utils/functions/stringToObjectId'
 
-import TaskModel from '../models/Task'
 import AssignmentTaskModel from '../models/AssignmentTask'
-import BoardModel from '../models/Board'
-import UserModel from '../models/User'
 
 export const insertAssignment = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { taskId, userId } = req.body
+    const { taskid, userid } = req.params
+    const assignmentTask = await AssignmentTaskModel.create({ task: stringToObjectId(taskid), user: stringToObjectId(userid) })
 
-    const task = await TaskModel.findOne({ _id: taskId })
-
-    if (!task) {
-      throw {
-        status: httpStatus.NOT_FOUND,
-        message: 'Task not found!',
-        error: 'Task not found!',
-      }
-    } else {
-      const board = await BoardModel.findOne({ _id: task.board })
-
-      if (!board) {
-        throw {
-          status: httpStatus.NOT_FOUND,
-          message: 'Board not found!',
-          error: 'Board not found!',
-        }
-      } else {
-        await AssignmentTaskModel.create({ task: taskId, user: userId })
-
-        const userAssigned = await UserModel.findOne({ _id: userId })
-
-        res.status(httpStatus.OK).send({
-          message: 'User assignments',
-          result: userAssigned,
-        })
-      }
-    }
+    res.status(httpStatus.OK).send({
+      message: 'Assignment task inserted!',
+      result: assignmentTask,
+    })
   } catch (err) {
     res.status(err.status ?? httpStatus.INTERNAL_SERVER_ERROR).send({
       message: err.message ?? 'Internal server erro on user login!',
@@ -50,33 +25,13 @@ export const insertAssignment = async (req: Request, res: Response): Promise<voi
 
 export const deleteAssignment = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { taskId, userId } = req.body
+    const { taskid, userid } = req.params
 
-    const task = await TaskModel.findOne({ _id: taskId })
+    await AssignmentTaskModel.findOneAndDelete({ task: stringToObjectId(taskid), user: stringToObjectId(userid) })
 
-    if (!task) {
-      throw {
-        status: httpStatus.NOT_FOUND,
-        message: 'Assignment not found',
-        error: 'Assignment not found',
-      }
-    } else {
-      const board = await BoardModel.findOne({ _id: task.board })
-
-      if (!board) {
-        throw {
-          status: httpStatus.NOT_FOUND,
-          message: 'Board not found',
-          error: 'Board not found',
-        }
-      } else {
-        await AssignmentTaskModel.findOneAndDelete({ task: taskId, user: userId })
-
-        res.status(httpStatus.OK).send({
-          message: 'Assignment task deleted!',
-        })
-      }
-    }
+    res.status(httpStatus.OK).send({
+      message: 'Assignment task deleted!',
+    })
   } catch (err) {
     res.status(err.status ?? httpStatus.INTERNAL_SERVER_ERROR).send({
       message: err.message ?? 'Internal server erro on user login!',
